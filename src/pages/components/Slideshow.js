08dd9a65a963b87816904cc00c5e326c8934c3f0
Lewis Taylor <lewistaylor@MacBook-Air.local>
2025-06-25 02:28:47 +0100

    a lot of styling complete

--- src/pages/components/Slideshow.js
+++ src/pages/components/Slideshow.js
@@ -31,7 +31,6 @@ export default function Slideshow() {
             layout="fill"
             objectFit="cover"
             priority={i === 0}
-            unoptimized
           />
         </div>
       ))}
